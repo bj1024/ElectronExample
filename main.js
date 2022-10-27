4951be7bec958a1270bd1ca63513864295d09e30
H.K <16552133+bj1024@users.noreply.github.com>
2022-10-27 11:51:14 +0900

    Local express server.

--- main.js
+++ main.js
@@ -1,7 +1,36 @@
 const { app, BrowserWindow } = require('electron')
 const { shell } = require('electron')
 
+const express = require('express');
+const exp = express();
+
+
+const yyyymmdd = new Intl.DateTimeFormat(
+  undefined,
+  {
+    year:   'numeric',
+    month:  '2-digit',
+    day:    '2-digit',
+    hour:   '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    fractionalSecondDigits: 3,
+  }
+)
+
+
+
+exp.use(express.static('public'));
+exp.get('/', function (req, res) {
+  // res.render(__dirname + '/index.html'); // render is template rendering.
+  
+  // res.status(200).send('hello.\nserver time=[' + yyyymmdd.format( new Date()) + ']')
+  res.status(200).send(`hello.<br>server time=[${yyyymmdd.format( new Date())}]`)
+});
+exp.listen(3000,"127.0.0.1");
+
 const createWindow = () => {
+  console.log("createWindow")
   const win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -11,7 +40,6 @@ const createWindow = () => {
     }
   })
 
-  // // リンクをクリックするとWebブラウザで開く
   // const handleUrlOpen = (e, url)=>{
   //   if( url.match(/^http/)){
   //     e.preventDefault()
@@ -19,7 +47,8 @@ const createWindow = () => {
   //   }
   // }
   // win.webContents.on('will-navigate', handleUrlOpen);  // 
-  
+
+  // target='_blank',window.opwn
     win.webContents.setWindowOpenHandler(({ url }) => {
       console.log("setWindowOpenHandler " + url)
       shell.openExternal(url);
@@ -37,6 +66,15 @@ app.whenReady().then(() => {
       createWindow();
     }
   });
+
+  // mainWindow = new BrowserWindow({width: 800, height: 600});
+    // ログインページにアクセスして、画面に表示する
+    // mainWindow.loadURL("http://localhost:3000/" );
+
+    // mainWindow.on('closed', function() {
+    //     mainWindow = null;
+    // });
+
 })
 
 app.on('window-all-closed', () => {
@@ -46,7 +84,3 @@ app.on('window-all-closed', () => {
   // }
 
 })
-
-
-
-
